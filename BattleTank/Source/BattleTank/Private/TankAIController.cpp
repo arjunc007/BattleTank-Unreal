ef73239228ce8d2d75e6f897e91455f35e9177ee
arjunc007 <arjunchauhan007@gmail.com>
2019-01-20 19:50:59 +0000

    AI Controllers

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player tank: %s"), *(PlayerTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto ppawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!ppawn)
+	{
+		return nullptr;
+	}
+	return Cast<ATank>(ppawn);
+}
